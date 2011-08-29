4d679bafccf6dd2d0a450ace8cc1ce8aca4b5540
Ryan Linnit <tswizzle@live.co.uk>
2011-08-29 19:19:12 +0100

    Improved Regex & other small things

--- soundcloud-downloader.py
+++ soundcloud-downloader.py
@@ -6,13 +6,13 @@ if len(sys.argv) <= 1:
 	exit("You need to enter a URI to download from")
 
 def get_dl_url(htmlsource):
-	regexp = "<img [^\>]*class=([\"\'])\waveform([\"\'])(.*?)>"
+	regexp = "<img\ class=([\"\'])\waveform([\"\'])(.*?)>"
 
 	for match in re.finditer(regexp, htmlsource):
 		image = repr(match.group())
 		break
 
-	id = image.split('src="')[1].split('"')[0].split('/')[3].split('_')[0]
+	id = m = re.split('http://(.*?)/(.*?)_m.png"', image)[2]
 
 	url = "http://media.soundcloud.com/stream/%s" % id
 
@@ -25,23 +25,27 @@ def get_title(htmlsource):
 		title = repr(match.group())
 		break
 	
-	title = title.split('<title>')[1].split('</title>')[0].split('on SoundCloud')[0]
+	title = re.split('<title>(.*?)</title>', title)[1].split(' on SoundCloud')[0]
+	
 	title = title.replace('by', '-')
-	return title + ".mp3"
+	return "%s.mp3" % title
 
 def main():
-	print "Getting Information"
+	print "Getting Information... "
 	soundcloud_url = sys.argv[-1]
-	html = urllib2.urlopen(soundcloud_url)
+	try:
+		html = urllib2.urlopen(soundcloud_url)
+	except ValueError:
+		exit("Error: The URL '%s' can not be retrieved" % soundcloud_url)
 	htmlsource = html.read()
 	html.close()
 	
 	title = get_title(htmlsource)
-	
 	url = get_dl_url(htmlsource)
 	
-	print "Downloading File..."
+	print "Downloading File '%s'" % title
 	urllib.urlretrieve(url, title)
+	print "Download Complete"
 
 if __name__ == '__main__':
 	main()
