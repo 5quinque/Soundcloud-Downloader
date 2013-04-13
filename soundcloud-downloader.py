1be0a0aec8edbe20a8e7118269d98aaeadf8c021
Ryan <tswizzle@live.co.uk>
2013-04-14 00:18:09 +0100

    Updated get_title() function

--- soundcloud-downloader.py
+++ soundcloud-downloader.py
@@ -1,11 +1,9 @@
 ﻿#!/usr/bin/python
 # 13/04/13
-import urllib2, sys, re
-import urllib
-#import urllib
+import urllib, urllib2, sys, re
 
 if len(sys.argv) <= 1:
-	exit("You need to enter a URI to download from")
+	exit("You need to enter a soundcloud URI\nI.E:\n $./ %s https://soundcloud/user/song" % sys.argv[0])
 
 def get_dl_url(htmlsource):
 	# regular expression for the string we will search for in htmlsource 
@@ -28,22 +26,17 @@ def get_dl_url(htmlsource):
 
 def get_title(htmlsource):
 	# regular expression for the string we will search for in htmlsource
-	regexp = "<title>(.*?)\son\sSoundCloud.*</title>"
+	regexp = "<title>([^by]*)by\s(\w*)\son\sSoundCloud.*</title>"
 
-	# find the first match that occurs, if any
-	first_match = re.search(regexp, htmlsource)
+	match = re.search(regexp, htmlsource)
 	
-	if first_match:
+	if match:
 		# if we found a match, retrieve the title of the song
-		title = first_match.group(1)
-		
-		# replace every occurrence of 'by' in the title with a hyphen
-		title = title.replace('by', '-')
+		title = "%s - %s.mp3" % (match.group(1), match.group(2)) #\1 Songtitle \2 Artist
 	else:
-		print "No title data for song at %s. Exiting." % sys.argv[1]
-		sys.exit()	
+		sys.exit("No title data for song at %s. Exiting." % sys.argv[1])
 	
-	return "%s.mp3" % title
+	return title
 
 def main():
 	print "Getting Information... "
@@ -72,7 +65,6 @@ def main():
 	
 	# copy the download URL's content (the full song) to a file 
 	# with a filename that matches our song's title
-	#urllib.urlretrieve(url, title)
 	filename, headers = urllib.urlretrieve(url=url, filename=title, reporthook=report)
 	print "\n\nDownload Complete"
 
@@ -80,7 +72,8 @@ def report(block_no, block_size, file_size):
 	global download_progress
 	download_progress += block_size
 	
-	sys.stdout.write("\rDownloading (%.2fMB/%.2fMB): %.2f%% / 100%%" % (download_progress/1024/1024.00, file_size/1024.00/1024.00, 100 * float(download_progress)/float(file_size)) )
+	sys.stdout.write("\rDownloading (%.2fMB/%.2fMB): %.2f%% / 100%%" 
+		% (download_progress/1024.00/1024.00, file_size/1024.00/1024.00, 100 * float(download_progress)/float(file_size)) )
 	sys.stdout.flush()
 
 if __name__ == '__main__':
